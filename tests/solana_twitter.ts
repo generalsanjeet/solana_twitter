ef47da54504fd315ba42eea7f2791ee84968f9d6
generalsanjeet <generalsanjeet@gmail.com>
2023-06-07 17:28:42 +0530

    first test case  written

--- tests/solana_twitter.ts
+++ tests/solana_twitter.ts
@@ -1,16 +1,30 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import {program} from "@coral-xyz/anchor/dist/cjs/native/system";
 import { SolanaTwitter } from "../target/types/solana_twitter";
 
 describe("solana_twitter", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>
 
-  const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
+  it("can send a new tweet", async () => {
+    const tweetKeyPair  =  anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    await program.methods.sendTweet("sanjeet's first tweet", "My first tweet")
+    .accounts({
+      myTweet: tweetKeyPair.publicKey,
+      senderOfTweet: program.provider.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([tweetKeyPair])
+    .rpc();
+
+    const tweetAccount = await program.account.tweetOnSolana.fetch(
+      tweetKeyPair.publicKey
+    );
+
+    console.log(tweetAccount);
+
   });
+
 });
